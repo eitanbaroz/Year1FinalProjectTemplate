348cd33b74a3e25715a448e05fe7b2f704f156f1
eitanbaroz <162023274+eitanbaroz@users.noreply.github.com>
2024-06-09 13:47:14 +0300

    Update Script.js

--- website/Script.js
+++ website/Script.js
@@ -3,27 +3,27 @@ import Cookies from "./_cookies";
 let Logout = document.getElementById("logout");
 
 // Get references to the "Add Task" links
-const addNewTaskLink = document.getElementById('add-new-task');
-const addInProgressTaskLink = document.getElementById('add-in-progress-task');
-const addDoneTaskLink = document.getElementById('add-done-task');
+// const addNewTaskLink = document.getElementById('add-new-task');
+// const addInProgressTaskLink = document.getElementById('add-in-progress-task');
+// const addDoneTaskLink = document.getElementById('add-done-task');
 
 
 // Add click event listeners to the "Add Task" links
-addNewTaskLink.addEventListener('click', handleAddTask);
-addInProgressTaskLink.addEventListener('click', handleAddTask);
-addDoneTaskLink.addEventListener('click', handleAddTask);
+// addNewTaskLink.addEventListener('click', handleAddTask);
+// addInProgressTaskLink.addEventListener('click', handleAddTask);
+// addDoneTaskLink.addEventListener('click', handleAddTask);
 
 
 /**@type {HTMLButtonElement} */
-let AddTask = document.getElementsByClassName('add-task-button');
+let AddTask = document.getElementsByClassName('add-task-btn')[0];
 
 AddTask.onclick = function () {
     if (Cookies.get("id") == undefined) {
         alert("log in first");
     }
-    else{
-    window.location.href = 'AddTask.html';
-}
+    else {
+        window.location.href = 'AddTask.html';
+    }
 }
 
 Logout.onclick = function () {
